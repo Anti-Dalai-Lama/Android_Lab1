64ac79ad34cf0732f0ebb8c531e1089e9d8f4a4a
Anti-Dalai-Lama <princeartik@gmail.com>
2016-10-23 00:56:55 +0300

    Initial commit

---
+++ app/src/main/java/com/blablaarthur/lab1/MainActivity.java
@@ -0,0 +1,51 @@
+package com.blablaarthur.lab1;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button color;
+    Button calc;
+    Button notes;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        color = (Button) findViewById(R.id.color);
+        calc = (Button) findViewById(R.id.calc);
+        notes = (Button) findViewById(R.id.notes);
+
+        color.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent = new Intent(MainActivity.this,
+                        ColorPicker.class);
+                startActivity(myIntent);
+            }
+        });
+
+        calc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent = new Intent(MainActivity.this,
+                        Calculator.class);
+                startActivity(myIntent);
+            }
+        });
+
+        notes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent = new Intent(MainActivity.this,
+                        ColorPicker.class);
+                startActivity(myIntent);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/blablaarthur/lab1/Notes.java
@@ -0,0 +1,19 @@
+package com.blablaarthur.lab1;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Артур on 04.10.2016.
+ */
+
+public class Notes extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.notes);
+
+
+    }
+}
